f44952ea6d58c812a9ad73efa8f7829c83f49d58
Benjamin <83666488+benjamints@users.noreply.github.com>
2022-04-02 23:11:04 +0700

    committed

--- 20127312.py
+++ 20127312.py
@@ -4,274 +4,128 @@ from tkinter import messagebox
 
 root = Tk()
 root.title('Tic Tac Toe')
-root.iconbitmap(
-    'C:/Users/Lenovo/OneDrive - VNU-HCMUS/Documents/Information Technology/Semester 5/Co So Tri Tue Nhan Tao/Tic Tac/logo_hcmus.ico')
-# root.geometry("1200x710")
-
-
-# X starts so True
-clicked = True
 count = 0
-
-# disable all the buttons
+player1 = 'X'
+player2 = 'O'
 
 
 def disable_all_buttons():
     for i in t:
         for j in i:
             j.config(state=DISABLED)
-    # b1.config(state=DISABLED)
-    # b2.config(state=DISABLED)
-    # b3.config(state=DISABLED)
-    # b4.config(state=DISABLED)
-    # b5.config(state=DISABLED)
-    # b6.config(state=DISABLED) 
-    # b7.config(state=DISABLED) 
-    # b8.config(state=DISABLED)
-    # b9.config(state=DISABLED)    
-
-# Check to see if someone won
 
 
 def check_win():
-    global winner
-    winner = False
-
-    # if b1["text"] == "X" and b2["text"] == "X" and b3["text"] == "X":
-    #     b1.config(bg="red")
-    #     b2.config(bg="red")
-    #     b3.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! X Wins!!!")
-    #     disable_all_buttons()
-    # elif b4["text"] == "X" and b5["text"] == "X" and b6["text"] == "X":
-    #     b4.config(bg="red")
-    #     b5.config(bg="red")
-    #     b6.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! X Wins!!!")
-    #     disable_all_buttons()
-    # elif b7["text"] == "X" and b8["text"] == "X" and b9["text"] == "X":
-    #     b7.config(bg="red")
-    #     b8.config(bg="red")
-    #     b9.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! X Wins!!!")
-    #     disable_all_buttons()
-    # elif b1["text"] == "X" and b4["text"] == "X" and b7["text"] == "X":
-    #     b1.config(bg="red")
-    #     b4.config(bg="red")
-    #     b7.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! X Wins!!!")
-    #     disable_all_buttons()
-    # elif b2["text"] == "X" and b5["text"] == "X" and b8["text"] == "X":
-    #     b2.config(bg="red")
-    #     b5.config(bg="red")
-    #     b8.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! X Wins!!!")
-    #     disable_all_buttons()
-    # elif b3["text"] == "X" and b6["text"] == "X" and b9["text"] == "X":
-    #     b3.config(bg="red")
-    #     b6.config(bg="red")
-    #     b9.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! X Wins!!!")
-    #     disable_all_buttons()
-    # elif b1["text"] == "X" and b5["text"] == "X" and b9["text"] == "X":
-    #     b1.config(bg="red")
-    #     b5.config(bg="red")
-    #     b9.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! X Wins!!!")
-    #     disable_all_buttons()
-    # elif b3["text"] == "X" and b5["text"] == "X" and b7["text"] == "X":
-    #     b3.config(bg="red")
-    #     b5.config(bg="red")
-    #     b7.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! X Wins!!!")
-    #     disable_all_buttons()
-
-    # elif b1["text"] == "O" and b2["text"] == "O" and b3["text"] == "O":
-    #     b1.config(bg="red")
-    #     b2.config(bg="red")
-    #     b3.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! O Wins!!!")
-    #     disable_all_buttons()
-    # elif b4["text"] == "O" and b5["text"] == "O" and b6["text"] == "O":
-    #     b4.config(bg="red")
-    #     b5.config(bg="red")
-    #     b6.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! O Wins!!!")
-    #     disable_all_buttons()
-    # elif b7["text"] == "O" and b8["text"] == "O" and b9["text"] == "O":
-    #     b7.config(bg="red")
-    #     b8.config(bg="red")
-    #     b9.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! O Wins!!!")
-    #     disable_all_buttons()
-    # elif b1["text"] == "O" and b4["text"] == "O" and b7["text"] == "O":
-    #     b1.config(bg="red")
-    #     b4.config(bg="red")
-    #     b7.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! O Wins!!!")
-    #     disable_all_buttons()
-    # elif b2["text"] == "O" and b5["text"] == "O" and b8["text"] == "O":
-    #     b2.config(bg="red")
-    #     b5.config(bg="red")
-    #     b8.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! O Wins!!!")
-    #     disable_all_buttons()
-    # elif b3["text"] == "O" and b6["text"] == "O" and b9["text"] == "O":
-    #     b3.config(bg="red")
-    #     b6.config(bg="red")
-    #     b9.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! O Wins!!!")
-    #     disable_all_buttons()
-    # elif b1["text"] == "O" and b5["text"] == "O" and b9["text"] == "O":
-    #     b1.config(bg="red")
-    #     b5.config(bg="red")
-    #     b9.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! O Wins!!!")
-    #     disable_all_buttons()
-    # elif b3["text"] == "O" and b5["text"] == "O" and b7["text"] == "O":
-    #     b3.config(bg="red")
-    #     b5.config(bg="red")
-    #     b7.config(bg="red")
-    #     winner = True
-    #     messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! O Wins!!!")
-    #     disable_all_buttons()
-
+    global count
     for i in range(3):
-        if t[i][0]["text"] != " " and t[i][0]["text"] == t[i][1]["text"] and t[i][0]["text"] == t[i][2]["text"]:
+        if t[i][0]["text"] != ' ' and t[i][0]["text"] == t[i][1]["text"] and t[i][0]["text"] == t[i][2]["text"]:
             t[i][0].config(bg="red")
             t[i][1].config(bg="red")
             t[i][2].config(bg="red")
             winner = True
             disable_all_buttons()
-            if t[i][0]["text"] == "X":
+            if t[i][0]["text"] == 'X':
                 messagebox.showinfo(
                     "Tic Tac Toe", "CONGRATULATIONS! X Wins!!!")
-                return 1
             else:
                 messagebox.showinfo(
                     "Tic Tac Toe", "CONGRATULATIONS! O Wins!!!")
-                return -1
-        if t[0][i]["text"] != " " and t[0][i]["text"] == t[1][i]["text"] and t[0][i]["text"] == t[2][i]["text"]:
+            return
+        if t[0][i]["text"] != ' ' and t[0][i]["text"] == t[1][i]["text"] and t[0][i]["text"] == t[2][i]["text"]:
             t[0][i].config(bg="red")
             t[1][i].config(bg="red")
             t[2][i].config(bg="red")
             disable_all_buttons()
             winner = True
-            if t[0][i]["text"] == "X":
+            if t[0][i]["text"] == 'X':
                 messagebox.showinfo(
                     "Tic Tac Toe", "CONGRATULATIONS! X Wins!!!")
-                return 1
             else:
                 messagebox.showinfo(
                     "Tic Tac Toe", "CONGRATULATIONS! O Wins!!!")
-                return -1
+            return
 
-    if t[0][0]["text"] != " " and t[1][1]["text"] == t[0][0]["text"] and t[0][0]["text"] == t[2][2]["text"]:
+    if t[0][0]["text"] != ' ' and t[1][1]["text"] == t[0][0]["text"] and t[0][0]["text"] == t[2][2]["text"]:
         t[0][0].config(bg="red")
         t[1][1].config(bg="red")
         t[2][2].config(bg="red")
         disable_all_buttons()
         winner = True
-        if t[0][0]["text"] == "X":
+        if t[0][0]["text"] == 'X':
             messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! X Wins!!!")
-            return 1
         else:
             messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! O Wins!!!")
-            return -1
+        return
 
-    if t[0][2]["text"] != " " and t[1][1]["text"] == t[0][2]["text"] and t[1][1]["text"] == t[2][0]["text"]:
+    if t[0][2]["text"] != ' ' and t[1][1]["text"] == t[0][2]["text"] and t[1][1]["text"] == t[2][0]["text"]:
         t[0][2].config(bg="red")
         t[1][1].config(bg="red")
         t[2][0].config(bg="red")
         disable_all_buttons()
         winner = True
-        if t[0][2]["text"] == "X":
+        if t[0][2]["text"] == 'X':
             messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! X Wins!!!")
-            return 1
         else:
             messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS! O Wins!!!")
-            return -1
+        return
 
     # Check if tie
-    if count == 9 and winner == False:
+    if count == 9:
         messagebox.showinfo("Tic Tac Toe", "It's A Tie!\n No One Wins!")
         disable_all_buttons()
         return 0
     return 2
 
 
-def minimax(board, depth, isMaximizing):
-    a = check_win()
-    if a != 2:
-        return a
+def checkWin(bot):
+    for i in range(3):
+        if t[i][0]["text"] == bot and t[i][0]["text"] == t[i][1]["text"] and t[i][0]["text"] == t[i][2]["text"]:
+            return True
+        if t[0][i]["text"] == bot and t[0][i]["text"] == t[1][i]["text"] and t[0][i]["text"] == t[2][i]["text"]:
+            return True
 
-    if isMaximizing == True:
-        bestScore = -10000
+    if t[0][0]["text"] == bot and t[1][1]["text"] == t[0][0]["text"] and t[0][0]["text"] == t[2][2]["text"]:
+        return True
+    if t[0][2]["text"] == bot and t[1][1]["text"] == t[0][2]["text"] and t[1][1]["text"] == t[2][0]["text"]:
+        return True
+    return False
+
+
+def minimax(board, depth, isMaximizing):
+    global count
+    if checkWin(player2):
+        return 1
+    if checkWin(player1):
+        return -1
+    if depth == 9:
+        return 0
+    if isMaximizing:
+        bestScore = -100
         for i in range(3):
             for j in range(3):
-                if board[i][j]["text"] == " ":
-                    board[i][j] = "X"
+                if t[i][j]["text"] == ' ':
+                    t[i][j]["text"] = player2
                     score = minimax(board, depth + 1, False)
-                    board[i][j]["text"] = " "
+                    t[i][j]["text"] = ' '
                     bestScore = max(score, bestScore)
         return bestScore
     else:
         bestScore = 10000
-        for i in t:
-            for j in i:
-                if j["text"] == " ":
-                    j["text"] = "X"
-                    score = minimax(board, depth + 1, False)
-                    j["text"] = " "
+        for i in range(3):
+            for j in range(3):
+                if t[i][j]["text"] == ' ':
+                    t[i][j]["text"] = player1
+                    score = minimax(board, depth + 1, True)
+                    t[i][j]["text"] = ' '
                     bestScore = min(score, bestScore)
         return bestScore
 
 
-def bestMove():
-    bestScore = -10000
-
-    for i in t:
-        for j in i:
-            if j["text"] == " ":
-                j["text"] == "X"
-                score = minimax(t, 0, False)
-                j["text"] == " "
-                if score > bestScore:
-                    bestScore = score
-                    move = [i, j]
-
-    t[move[0]][move[1]]["text"] = "X"
-    # button[move[0]][move[1]].config(text=comp)
-# Button clicked functionality
-
-
 def b_click(b):
-    global clicked, count
-    if b["text"] == " " and clicked == True:
-        b["text"] = "X"
-        clicked = False
-        count += 1
-        b.config(state=DISABLED)
-        check_win()
-    elif b["text"] == " " and clicked == False:  
-        b["text"] = "O"
-        clicked = True
-        count += 1
+    global count
+    if b["text"] == ' ':
+        b["text"] = player1
+        count = count + 1
         b.config(state=DISABLED)
         check_win()
     else:
@@ -279,50 +133,71 @@ def b_click(b):
             "Tic Tac Toe", "Hey! That box has already been selected\nPick Another Box...")
         b.config(state=DISABLED)
 
-# Start the game over!
+
+def botMove():
+    global count, t, player1, player2
+    bestScore = -100
+    bestMove = t[0][0]
+    for i in t:
+        for j in i:
+            if (j["text"] == ' '):
+                j["text"] = player2
+                score = minimax(t, count + 1, False)
+                j["text"] = ' '
+                if (score > bestScore):
+                    bestScore = score
+                    bestMove = j
+    bestMove["text"] = player2
+    bestMove.config(state=DISABLED)
+    count = count + 1
+    check_win()
+
+
+def start_commad(button):
+    b_click(button)
+    botMove()
 
 
 def reset():
+    global count
+    global player1, player2
     global t
-    global clicked, count
-    clicked = True
     count = 0
+    role = messagebox.askquestion(
+        "Start a new game", "Do you want to go first?")
+    if role == "yes":
+        player1 = 'O'
+        player2 = 'X'
 
-    # Build our buttons
-    b1 = Button(root, text=" ", font=("Helvetica", 25), height=3,
-                width=6, bg="SystemButtonFace", command=lambda: b_click(b1))
-    b2 = Button(root, text=" ", font=("Helvetica", 25), height=3,
-                width=6, bg="SystemButtonFace", command=lambda: b_click(b2))
-    b3 = Button(root, text=" ", font=("Helvetica", 25), height=3,
-                width=6, bg="SystemButtonFace", command=lambda: b_click(b3))
+    # Build our buttons                width=6, bg="SystemButtonFace", command=lambda: start_commad(t[i][j]))
+    b1 = Button(root, text=' ', font=("Helvetica", 25), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(b1))
+    b2 = Button(root, text=' ', font=("Helvetica", 25), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(b2))
+    b3 = Button(root, text=' ', font=("Helvetica", 25), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(b3))
 
-    b4 = Button(root, text=" ", font=("Helvetica", 25), height=3,
-                width=6, bg="SystemButtonFace", command=lambda: b_click(b4))
-    b5 = Button(root, text=" ", font=("Helvetica", 25), height=3,
-                width=6, bg="SystemButtonFace", command=lambda: b_click(b5))
-    b6 = Button(root, text=" ", font=("Helvetica", 25), height=3,
-                width=6, bg="SystemButtonFace", command=lambda: b_click(b6))
+    b4 = Button(root, text=' ', font=("Helvetica", 25), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(b4))
+    b5 = Button(root, text=' ', font=("Helvetica", 25), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(b5))
+    b6 = Button(root, text=' ', font=("Helvetica", 25), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(b6))
 
-    b7 = Button(root, text=" ", font=("Helvetica", 25), height=3,
-                width=6, bg="SystemButtonFace", command=lambda: b_click(b7))
-    b8 = Button(root, text=" ", font=("Helvetica", 25), height=3,
-                width=6, bg="SystemButtonFace", command=lambda: b_click(b8))
-    b9 = Button(root, text=" ", font=("Helvetica", 25), height=3,
-                width=6, bg="SystemButtonFace", command=lambda: b_click(b9))
-
-    # Grid our butto     ns to the screen
-    b1.grid(row=0, column=0)
-    b2.grid(row=0, column=1)
-    b3.grid(row=0, column=2)
-
-    b4.grid(row=1, column=0)
-    b5.grid(row=1, column=1)
-    b6.grid(row=1, column=2)
-
-    b7.grid(row=2, column=0)
-    b8.grid(row=2, column=1)
-    b9.grid(row=2, column=2)
+    b7 = Button(root, text=' ', font=("Helvetica", 25), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(b7))
+    b8 = Button(root, text=' ', font=("Helvetica", 25), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(b8))
+    b9 = Button(root, text=' ', font=("Helvetica", 25), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(b9))
     t = [[b1, b2, b3], [b4, b5, b6], [b7, b8, b9]]
+    if player1 == 'X':
+        botMove()
+    # Grid our buttons to the screen
+    for i in range(3):
+        for j in range(3):
+            t[i][j].grid(row=i, column=j)
+
 
 # Create my menu
 my_menu = Menu(root)
--- constants.py
+++ constants.py
@@ -1,66 +1,197 @@
-def printBoard(board):
-    print(board[1] + '|' + board[2] + '|' + board[3])
-    print('-+-+-')
-    print(board[4] + '|' + board[5] + '|' + board[6])
-    print('-+-+-')
-    print(board[7] + '|' + board[8] + '|' + board[9])
-    print("\n")
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.title('Tic-Tac-Toe')
 
 
-def spaceIsFree(position):
+def b_click(b):
+    if b["text"] == " ":
+        b["text"] = player
+        return True
+    else:
+        return False
+
+def botauto(b):
+    b["text"] = bot
+
+
+def start_commad(number):
+    if number == 1:
+        if (b_click(Button_1) == True):
+            addMove(player, 1)
+            botMove()
+        else:
+            messagebox.showerror("Warning", "Already selected!\n")
+    if number == 2:
+        if (b_click(Button_2) == True):
+            addMove(player, 2)
+            botMove()
+        else:
+            messagebox.showerror("Warning", "Already selected!\n")
+    if number == 3:
+        if (b_click(Button_3) == True):
+            addMove(player, 3)
+            botMove()
+            
+        else:
+            messagebox.showerror("Warning", "Already selected!\n")
+    if number == 4:
+        if (b_click(Button_4) == True):
+            addMove(player, 4)
+            botMove()
+            
+        else:
+            messagebox.showerror("Warning", "Already selected!\n")
+    if number == 5:
+        if (b_click(Button_5) == True):
+            addMove(player, 5)
+            botMove()
+            
+        else:
+            messagebox.showerror("Warning", "Already selected!\n")
+    if number == 6:
+        if (b_click(Button_6) == True):
+            addMove(player, 6)
+            botMove()
+        else:
+            messagebox.showerror("Warning", "Already selected!\n")
+    if number == 7:
+        if (b_click(Button_7) == True):
+            addMove(player, 7)
+            botMove()
+        else:
+            messagebox.showerror("Warning", "Already selected!\n")
+    if number == 8:
+        if (b_click(Button_8) == True):
+            addMove(player, 8)
+            botMove()
+        else:
+            messagebox.showerror("Warning", "Already selected!\n")
+    if number == 9:
+        if (b_click(Button_9) == True):
+            addMove(player, 9)
+            botMove()
+            
+        else:
+            messagebox.showerror("Warning", "Already selected!\n")
+
+
+def play_game():
+    global board
+    global player 
+    global bot
+    global Button_1, Button_2, Button_3, Button_4, Button_5, Button_6, Button_7, Button_8, Button_9
+    board = {1: ' ', 2: ' ', 3: ' ',
+             4: ' ', 5: ' ', 6: ' ',
+             7: ' ', 8: ' ', 9: ' '}
+    role = messagebox.askquestion("Start a new game", "Do you want to go first?")
+    if role == 'yes':
+        player = 'O'
+        bot = 'X'
+    else:
+        player = 'X'
+        bot = 'O' 
+
+
+    Button_1 = Button(root, text=" ", font=("Helvetica", 30), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(1))
+    Button_2 = Button(root, text=" ", font=("Helvetica", 30), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(2))
+    Button_3 = Button(root, text=" ", font=("Helvetica", 30), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(3))
+    Button_4 = Button(root, text=" ", font=("Helvetica", 30), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(4))
+    Button_5 = Button(root, text=" ", font=("Helvetica", 30), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(5))
+    Button_6 = Button(root, text=" ", font=("Helvetica", 30), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(6))
+    Button_7 = Button(root, text=" ", font=("Helvetica", 30), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(7))
+    Button_8 = Button(root, text=" ", font=("Helvetica", 30), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(8))
+    Button_9 = Button(root, text=" ", font=("Helvetica", 30), height=3,
+                width=6, bg="SystemButtonFace", command=lambda: start_commad(9))
+    if (bot == 'O'):
+        botMove()
+
+    # Grid
+    Button_1.grid(row=0, column=0)
+    Button_2.grid(row=0, column=1)
+    Button_3.grid(row=0, column=2)
+
+    Button_4.grid(row=1, column=0)
+    Button_5.grid(row=1, column=1)
+    Button_6.grid(row=1, column=2)
+
+    Button_7.grid(row=2, column=0)
+    Button_8.grid(row=2, column=1)
+    Button_9.grid(row=2, column=2)
+
+def Check_Space(position):
     if board[position] == ' ':
         return True
     else:
         return False
 
-
-def insertLetter(letter, position):
-    if spaceIsFree(position):
-        board[position] = letter
-        printBoard(board)
+def addMove(bot_or_player, position):
+    if Check_Space(position):
+        board[position] = bot_or_player
         if (checkDraw()):
-            print("Draw!")
-            exit()
-        if checkForWin():
-            if letter == 'X':
-                print("Bot wins!")
-                exit()
+            messagebox.showinfo("Tic Tac Toe", "Draw!!")
+        if checkWin():
+            if bot_or_player == bot:
+                messagebox.showinfo("Tic Tac Toe", "You fail, bot wins!!")
             else:
-                print("Player wins!")
-                exit()
-
+                messagebox.showinfo("Tic Tac Toe", "Congratulations, you wins!!")
         return
 
-
-    else:
-        print("Can't insert there!")
-        position = int(input("Please enter new position:  "))
-        insertLetter(letter, position)
-        return
-
-
-def checkForWin():
+def checkWin():
     if (board[1] == board[2] and board[1] == board[3] and board[1] != ' '):
+        Button_1.config(bg="red")
+        Button_2.config(bg="red")
+        Button_3.config(bg="red")
         return True
     elif (board[4] == board[5] and board[4] == board[6] and board[4] != ' '):
+        Button_4.config(bg="red")
+        Button_5.config(bg="red")
+        Button_6.config(bg="red")
         return True
     elif (board[7] == board[8] and board[7] == board[9] and board[7] != ' '):
+        Button_7.config(bg="red")
+        Button_8.config(bg="red")
+        Button_9.config(bg="red")
         return True
     elif (board[1] == board[4] and board[1] == board[7] and board[1] != ' '):
+        Button_1.config(bg="red")
+        Button_4.config(bg="red")
+        Button_7.config(bg="red")
         return True
     elif (board[2] == board[5] and board[2] == board[8] and board[2] != ' '):
+        Button_2.config(bg="red")
+        Button_5.config(bg="red")
+        Button_8.config(bg="red")
         return True
     elif (board[3] == board[6] and board[3] == board[9] and board[3] != ' '):
+        Button_3.config(bg="red")
+        Button_6.config(bg="red")
+        Button_9.config(bg="red")
         return True
     elif (board[1] == board[5] and board[1] == board[9] and board[1] != ' '):
+        Button_1.config(bg="red")
+        Button_5.config(bg="red")
+        Button_9.config(bg="red")
         return True
     elif (board[7] == board[5] and board[7] == board[3] and board[7] != ' '):
+        Button_3.config(bg="red")
+        Button_5.config(bg="red")
+        Button_7.config(bg="red")
         return True
     else:
         return False
 
 
-def checkWhichMarkWon(mark):
+def checkWin_tocalc(mark):
     if board[1] == board[2] and board[1] == board[3] and board[1] == mark:
         return True
     elif (board[4] == board[5] and board[4] == board[6] and board[4] == mark):
@@ -85,41 +216,18 @@ def checkDraw():
     for key in board.keys():
         if (board[key] == ' '):
             return False
-    return True       # TOD
-
-
-def playerMove():
-    position = int(input("Enter the position for 'O':  "))
-    insertLetter(player, position)
-    return
-
-
-def compMove():
-    bestScore = -800
-    bestMove = 0
-    for key in board.keys():
-        if (board[key] == ' '):
-            board[key] = bot
-            score = minimax(board, 0, False)
-            board[key] = ' '
-            if (score > bestScore):
-                bestScore = score
-                bestMove = key
-
-    insertLetter(bot, bestMove)
-    return
-
+    return True
 
 def minimax(board, depth, isMaximizing):
-    if (checkWhichMarkWon(bot)):
+    if (checkWin_tocalc(bot)):
         return 1
-    elif (checkWhichMarkWon(player)):
+    elif (checkWin_tocalc(player)):
         return -1
     elif (checkDraw()):
         return 0
 
     if (isMaximizing):
-        bestScore = -800
+        bestScore = -10
         for key in board.keys():
             if (board[key] == ' '):
                 board[key] = bot
@@ -130,35 +238,56 @@ def minimax(board, depth, isMaximizing):
         return bestScore
 
     else:
-        bestScore = 800
+        bestScore = 10
         for key in board.keys():
             if (board[key] == ' '):
                 board[key] = player
                 score = minimax(board, depth + 1, True)
+
                 board[key] = ' '
                 if (score < bestScore):
                     bestScore = score
         return bestScore
 
 
-board = {1: ' ', 2: ' ', 3: ' ',
-         4: ' ', 5: ' ', 6: ' ',
-         7: ' ', 8: ' ', 9: ' '}
+def botMove():
+    bestScore = -10
+    bestMove = 0
+    for key in board.keys():
+        if (board[key] == ' '):
+            board[key] = bot
+            score = minimax(board, 0, False)
+            board[key] = ' '
+            if (score > bestScore):
+                bestScore = score
+                bestMove = key
+    if (bestMove == 1):
+        Button_1["text"] = bot
+    if (bestMove == 2):
+        Button_2["text"] = bot
+    if (bestMove == 3):
+        Button_3["text"] = bot
+    if (bestMove == 4):
+        Button_4["text"] = bot
+    if (bestMove == 5):
+        Button_5["text"] = bot
+    if (bestMove == 6):
+        Button_6["text"] = bot
+    if (bestMove == 7):
+        Button_7["text"] = bot
+    if (bestMove == 8):
+        Button_8["text"] = bot
+    if (bestMove == 9):
+        Button_9["text"] = bot    
+    addMove(bot, bestMove)
+    return
 
-printBoard(board)
-print("Computer goes first! Good luck.")
-print("Positions are as follow:")
-print("1, 2, 3 ")
-print("4, 5, 6 ")
-print("7, 8, 9 ")
-print("\n")
-player = 'O'
-bot = 'X'
+my_menu = Menu(root)
+root.config(menu=my_menu)
 
+options_menu = Menu(my_menu)
+my_menu.add_cascade(label="Options", menu=options_menu)
+options_menu.add_command(label="Reset Game", command=play_game)
 
-global firstComputerMove
-firstComputerMove = True
-
-while not checkForWin():
-    compMove()
-    playerMove()
+play_game()
+root.mainloop()
